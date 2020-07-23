43ab1357d56854c61a42f4b51638edd962b33705
Tarkan Sarıtaş <tarkansaritas99@hotmail.com>
2020-07-23 22:22:04 +0300

    Main Page

---
+++ index.php
@@ -0,0 +1,81 @@
+<html>
+	<head>
+		<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+		<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+		<link rel="stylesheet" href="index.css">
+		<link rel="stylesheet" href="card.css">
+			<script src="https://kit.fontawesome.com/d6d9da6692.js" crossorigin="anonymous"></script>
+	<script src="bootstrap/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+	</head>
+	<body>
+		<div class="pos-f-t">
+			<div class="collapse" id="navbarToggleExternalContent">
+				<div class="bg-dark" >
+					<div class="container">
+						<div class="row">
+						
+							<div class="col-sm-8 col-md-7 py-4">
+							
+							  <h4 class="text-white">Hakkımızda</h4>
+							  <span class="text-muted">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer faucibus accumsan tempor.
+							  Pellentesque nec lorem non diam molestie suscipit et eget eros. Ut sed suscipit ex. Cras eu elementum est. Quisque eu lobortis nisl, 
+							  et accumsan felis. Donec in volutpat ex. Etiam tempus malesuada neque ultrices mattis. Nunc eget dignissim magna. Curabitur sagittis 
+							  tristique odio, in pellentesque nisl. Vivamus convallis dui a cursus sollicitudin. Morbi quis erat vitae enim maximus vulputate. Sed at 
+							  justo turpis.</span>
+							
+							</div>
+							<div class="col-sm-4 offset-md-1 py-4">
+							  <h4 class="text-white">Contact</h4>
+							  <ul class="list-unstyled">
+								<li><a href="#" class="text-white">Follow on Twitter   <i class="fab fa-twitter"></i></a></li>
+								<li><a href="#" class="text-white">Like on Facebook    <i class="fab fa-facebook"></i></a></li>
+								<li><a href="#" class="text-white">Email me			   <i class="fas fa-envelope-square"></i></a></li>
+							  </ul>
+							</div>
+					  </div>
+				  </div>
+			  </div>
+		  </div>
+		  <nav class="navbar navbar-dark bg-dark">
+			  <div class="container">
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+				  <span class="navbar-toggler-icon"></span>
+				</button>
+			  </nav>
+		  </div>
+		</div>
+		<div class="container-fluid">
+			<div class="o-container--page">
+				<div class="c-information-area-title" >
+					<h2 class="area-title">Madde bağımlılığı nedir?</h2>
+				</div>
+			</div>
+				<div class="c-information-area">
+					<div class="c-information-area-content">
+						<p class="area-write">
+							Madde bağımlılığı, vücudun işlevlerini olumsuz yönde etkileyen maddelerin kullanılması, 
+							bundan dolayı zarar görüldüğü hâlde bu maddelerin kullanımının bırakılamamasıdır.
+							Bağımlı, madde kullanımına ara verdiğinde yoksunluk belirtileri yaşar. Zamanla madde kullanım 
+							sıklığını ve dozunu artırır.
+						</p>
+					</div>
+					
+				</div>
+				<img alt="Bağımlılık Nedir" class="c-information-img" src="resources/madde-bagimliligi.png">
+			</div>
+		</div>
+		
+		<div class="container-fluid">
+			<a class="card1" href="#">
+			  <h3 class="card-h3">This is option 1</h3>
+			  <p class="small card-p">Card description with lots of great facts and interesting details.</p>
+			  <div class="go-corner" href="#">
+			  </div>
+			</a>
+		</div>
+	</body>
+</html>
+
