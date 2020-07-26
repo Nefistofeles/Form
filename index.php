dbed92b2d1d6ca0ddf82549124e525f440306eee
Tarkan Sarıtaş <tarkansaritas99@hotmail.com>
2020-07-26 16:20:11 +0300

    Main Page extras

--- index.php
+++ index.php
@@ -5,77 +5,153 @@
 		<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+		<script src="https://kit.fontawesome.com/d6d9da6692.js" crossorigin="anonymous"></script>
+		
 		<link rel="stylesheet" href="index.css">
-		<link rel="stylesheet" href="card.css">
-			<script src="https://kit.fontawesome.com/d6d9da6692.js" crossorigin="anonymous"></script>
-	<script src="bootstrap/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+		<link rel="stylesheet" href="SelectedCard.css">
+		<!--<script src="bootstrap/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>-->
+	
 	</head>
 	<body>
-		<div class="pos-f-t">
-			<div class="collapse" id="navbarToggleExternalContent">
-				<div class="bg-dark" >
-					<div class="container">
-						<div class="row">
-						
-							<div class="col-sm-8 col-md-7 py-4">
+		<header>
+			<div class="pos-f-t">
+				<div class="collapse" id="navbarToggleExternalContent">
+					<div class="bg-dark" >
+						<div class="container">
+							<div class="row">
 							
-							  <h4 class="text-white">Hakkımızda</h4>
-							  <span class="text-muted">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer faucibus accumsan tempor.
-							  Pellentesque nec lorem non diam molestie suscipit et eget eros. Ut sed suscipit ex. Cras eu elementum est. Quisque eu lobortis nisl, 
-							  et accumsan felis. Donec in volutpat ex. Etiam tempus malesuada neque ultrices mattis. Nunc eget dignissim magna. Curabitur sagittis 
-							  tristique odio, in pellentesque nisl. Vivamus convallis dui a cursus sollicitudin. Morbi quis erat vitae enim maximus vulputate. Sed at 
-							  justo turpis.</span>
-							
-							</div>
-							<div class="col-sm-4 offset-md-1 py-4">
-							  <h4 class="text-white">Contact</h4>
-							  <ul class="list-unstyled">
-								<li><a href="#" class="text-white">Follow on Twitter   <i class="fab fa-twitter"></i></a></li>
-								<li><a href="#" class="text-white">Like on Facebook    <i class="fab fa-facebook"></i></a></li>
-								<li><a href="#" class="text-white">Email me			   <i class="fas fa-envelope-square"></i></a></li>
-							  </ul>
-							</div>
+								<div class="col-sm-8 col-md-7 py-4">
+								
+								  <h4 class="text-white">Hakkımızda</h4>
+								  <span class="text-muted">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer faucibus accumsan tempor.
+								  Pellentesque nec lorem non diam molestie suscipit et eget eros. Ut sed suscipit ex. Cras eu elementum est. Quisque eu lobortis nisl, 
+								  et accumsan felis. Donec in volutpat ex. Etiam tempus malesuada neque ultrices mattis. Nunc eget dignissim magna. Curabitur sagittis 
+								  tristique odio, in pellentesque nisl. Vivamus convallis dui a cursus sollicitudin. Morbi quis erat vitae enim maximus vulputate. Sed at 
+								  justo turpis.</span>
+								
+								</div>
+								<div class="col-sm-4 offset-md-1 py-4">
+								  <h4 class="text-white">Contact</h4>
+								  <ul class="list-unstyled">
+									<li><a href="#" class="text-white">Follow on Twitter   <i class="fab fa-twitter"></i></a></li>
+									<li><a href="#" class="text-white">Like on Facebook    <i class="fab fa-facebook"></i></a></li>
+									<li><a href="#" class="text-white">Email me			   <i class="fas fa-envelope-square"></i></a></li>
+								  </ul>
+								</div>
+						  </div>
 					  </div>
 				  </div>
 			  </div>
-		  </div>
-		  <nav class="navbar navbar-dark bg-dark">
-			  <div class="container">
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-				  <span class="navbar-toggler-icon"></span>
-				</button>
-			  </nav>
-		  </div>
-		</div>
-		<div class="container-fluid">
-			<div class="o-container--page">
-				<div class="c-information-area-title" >
-					<h2 class="area-title">Madde bağımlılığı nedir?</h2>
-				</div>
+			  <nav class="navbar navbar-dark bg-dark">
+				  <div class="container">
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+					  <span class="navbar-toggler-icon"></span>
+					</button>
+				  </nav>
+			  </div>
 			</div>
-				<div class="c-information-area">
-					<div class="c-information-area-content">
-						<p class="area-write">
-							Madde bağımlılığı, vücudun işlevlerini olumsuz yönde etkileyen maddelerin kullanılması, 
-							bundan dolayı zarar görüldüğü hâlde bu maddelerin kullanımının bırakılamamasıdır.
-							Bağımlı, madde kullanımına ara verdiğinde yoksunluk belirtileri yaşar. Zamanla madde kullanım 
-							sıklığını ve dozunu artırır.
-						</p>
+		</header>
+		<main>
+			<section>
+				<div class="container-fluid" style="position : relative ; height:380px">
+					<div class="o-container--page">
+						<div class="c-information-area-title" style="padding-left : 4rem ; padding-top : 2rem ;">
+							<h2 style="area-title" >Madde bağımlılığı nedir?</h2>
+						</div>
+					</div>
+						<div class="c-information-area">
+							<div class="c-information-area-content">
+								<p class="area-write" style="padding-left : 4rem ;">
+									Madde bağımlılığı, vücudun işlevlerini olumsuz yönde etkileyen maddelerin kullanılması, 
+									bundan dolayı zarar görüldüğü hâlde bu maddelerin kullanımının bırakılamamasıdır.
+									Bağımlı, madde kullanımına ara verdiğinde yoksunluk belirtileri yaşar. Zamanla madde kullanım 
+									sıklığını ve dozunu artırır.
+								</p>
+							</div>
+							
+						</div>
+						<img alt="Bağımlılık Nedir" class="c-information-img" src="resources/madde-bagimliligi.png">
+					</div>
+				</div>
+				<div class="container" style="padding : 2rem ;>
+					<div class="o-container o-container-page">
+						<div style="margin-bottom: 3rem;">
+							<h2 style="color:00ae42;">Aşağıdakilerden sadece 3'ünün 12 aylık bir süre içerisinde görüldüğü kişi bağımlıdır</h2>
+						</div>
+						<div style="box-sizing: inherit;">
+							<ol style="padding-right : 4rem ;">
+								<li class="list-group-item  justify-content-between align-items-center border border-success" style="font-size : 1.2rem">Kullanılan madde miktarının sorunlara rağmen giderek artırılması.</li>
+								<li class="list-group-item d-flex justify-content-between align-items-center " style="font-size : 1.2rem">Bırakma çabalarının boşa çıkması.</li>
+								<li class="list-group-item d-flex justify-content-between align-items-center border border-success" style="font-size : 1.2rem">Maddeyi sağlamak, kullanmak veya bırakmak için çok fazla zaman harcanması.</li>
+								<li class="list-group-item d-flex justify-content-between align-items-center " style="font-size : 1.2rem">Sosyal, mesleki ve kişisel etkinliklerin azaltılması veya bırakılması</li>
+							</ol>
+						</div>
+					</div>
+				</div>
+				<div class="container" style="padding : 2rem ;">
+					<div class="o-container o-container-page">
+						<div style="margin-bottom: 3rem;">
+							<h2 style="color:00ae42;">Etkileri</h2>
+							<p style="font-size : 1.2rem">Uyuşturucu olarak kullanılan maddelerin kimyasal yapıları birbirinden farklıdır. Kullanıldıklarında merkezi sinir sisteminin farklı bölümlerini 
+							etkileyerek fiziksel ve psikolojik tahribata yol açarlar. Uyuşturucu maddelerin hiçbir güvenli kullanım şekli yoktur. Kullanan herkes için bağımlı 
+							olma riski eşittir. Hücrelerimiz vücuda giren her maddeyi tanır ve bir daha unutmamak üzere hafızasına alır. Hücresel öğrenme süreci denen bu durum herkes için geçerlidir.</p>
+						</div>
+						<div style="box-sizing: inherit;">
+							<ol style="padding-right : 4rem ;">
+								<li class="list-group-item d-flex justify-content-between align-items-center border border-success" style="font-size : 1.2rem">Aklı ve iradeyi işlemez hale getirir. Kişiyi normal yaşam ve davranışlarından uzaklaştırır.</li>
+								<li class="list-group-item d-flex justify-content-between align-items-center" style="font-size : 1.2rem">Bulantı, kusma, karın ağrıları, kabızlık, ishal, mide ve bağırsak spazmlarına/kanamalarına sebep olur.</li>
+								<li class="list-group-item d-flex justify-content-between align-items-center border border-success" style="font-size : 1.2rem">Tüm iç organların zarar görmesine ve buna eşlik eden bir dizi hastalığa neden olur.</li>
+								<li class="list-group-item d-flex justify-content-between align-items-center" style="font-size : 1.2rem">Zehirlenmelere ve bu yolla gelen ölümlere sebep olur.</li>
+								<li class="list-group-item d-flex justify-content-between align-items-center border border-success" style="font-size : 1.2rem">Uyuşturucular, bireyin çevreye uyum yeteneğini azaltır. Bağımlı giderek aileden ve çevresinden kopararak, yalnızlaşır. Çoğu zaman bu tabloya ağır bunalımlar eşlik eder.</li>
+							</ol>
+						</div>
+					</div>
+				</div>
+				<div class="container" style="padding : 2rem ;">
+					<div class="o-container o-container-page">
+						<div style="margin-bottom: 3rem;">
+							<h2 style="color:00ae42;">Ne yapmalı?</h2>
+							
+						</div>
+						<div style="box-sizing: inherit;">
+							<ol style="padding-right : 4rem ;">
+								<li class="list-group-item d-flex justify-content-between align-items-center border border-success" style="font-size : 1.2rem">Eğer kişi maddenin etkisi altında ise onunla bu durumda konuşmanın yararı olmaz.</li>
+								<li class="list-group-item d-flex justify-content-between align-items-center" style="font-size : 1.2rem">Kendinizi hazır hissetmeden onunla konuşmayın.</li>
+								<li class="list-group-item d-flex justify-content-between align-items-center border border-success" style="font-size : 1.2rem">Açık, samimi ve inandırıcı olun, öğüt vermeyin.</li>
+								<li class="list-group-item d-flex justify-content-between align-items-center" style="font-size : 1.2rem">Genellemeler yapmaktan kaçının.</li>
+								<li class="list-group-item d-flex justify-content-between align-items-center border border-success" style="font-size : 1.2rem">Korkularınıza dayanarak konuşmayın.</li>
+								<li class="list-group-item d-flex justify-content-between align-items-center" style="font-size : 1.2rem">Onu etiketlemekten kaçının, çünkü “kullanıcı olarak” etiketlenen kişiye yaklaşmak çok zordur.</li>
+								<li class="list-group-item d-flex justify-content-between align-items-center border border-success" style="font-size : 1.2rem">Önyargılarınızın farkına varın (“Bunlar iflah olmaz”), böylece yanlış iletişim kurma olasılığını azaltırsınız.</li>
+								<li class="list-group-item d-flex justify-content-between align-items-center" style="font-size : 1.2rem">Kendinizi onun yerine koymayı deneyerek onun düşünce, yaşantı ve korkularını anlamaya çalışın.</li>
+								<li class="list-group-item d-flex justify-content-between align-items-center border border-success" style="font-size : 1.2rem">Uzman yardımı alması için samimi bir yaklaşımla onu ikna edin.</li>
+							</ol>
+						</div>
 					</div>
 					
 				</div>
-				<img alt="Bağımlılık Nedir" class="c-information-img" src="resources/madde-bagimliligi.png">
-			</div>
-		</div>
-		
-		<div class="container-fluid">
-			<a class="card1" href="#">
-			  <h3 class="card-h3">This is option 1</h3>
-			  <p class="small card-p">Card description with lots of great facts and interesting details.</p>
-			  <div class="go-corner" href="#">
-			  </div>
-			</a>
-		</div>
+				<div class="container btn-toolbar" style="padding-left : 3.9rem ; ">
+					<a class="card" href="#">
+						<div class="id">
+							<h3 class="card-p">This is option 1</h3>
+							<p class="small card-h">Card description with lots of great facts and interesting details.</p>
+						</div>
+					</a>
+					<a class="card" href="#">
+						<div class="id">
+							<h3 class="card-p">This is option 1</h3>
+							<p class="small card-h">Card description with lots of great facts and interesting details.</p>
+						</div>
+					</a>
+					<a class="card" href="#">
+						<div class="id">
+								<h3 class="card-p">This is option 1</h3>
+							<p class="small card-h">Card description with lots of great facts and interesting details.</p>
+						</div>
+					</a>
+				</div>
+			</section>
+		</main>
+		<footer></footer>
 	</body>
 </html>
 
