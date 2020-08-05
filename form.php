30ecf82cf1af102c69c0bf28ca21ad1271d341ba
Tarkan Sarıtaş <tarkansaritas99@hotmail.com>
2020-08-05 16:32:35 +0300

    form page basics

--- form.php
+++ form.php
@@ -2,13 +2,60 @@
 
 <html>
 	<head>
-	
+		<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+		<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+		<script src="https://kit.fontawesome.com/d6d9da6692.js" crossorigin="anonymous"></script>
+		
+		<link rel="stylesheet" href="index.css">
+		<link rel="stylesheet" href="SelectedCard.css">
 	</head>
 	<body>
-		<form action="includes/formdata.inc.php" method="POST">
-			<input type="text" name="tag" placeholder="tag">
+	<!-- action="includes/formdata.inc.php" -->
+		<form action="form.php" method="POST">
+			<input type="number" name="number" placeholder="number">
 			<button type="submit" name="submit">Find</button>
 		</form>
-
+		
+		<?php
+			include_once 'includes/databaseConnection.inc.php';
+			
+			$number = mysqli_real_escape_string($conn, $_POST['number']) ;
+			$sql = "select * from question_option 
+			inner join question on question.id = question_option.question_id 
+			inner join form on question.form_id = '$number';";
+			
+			
+			$result = $conn->query($sql) ;
+		?>
+		
+		<?php 
+			echo '<ul class="list-group">' ;
+			$question_data = "";
+			$question_number = 1 ;
+			  while($row = $result->fetch_assoc()){
+				
+				$question_string = $row["question_string"] ;
+				if(strcmp ($question_string , $question_data) == 0){
+				
+				}else{
+					echo '<li class="list-group-item">'.$question_number.'. '.$question_string.'</li>' ;
+					$question_data = $question_string ;
+					$question_number++ ;
+				}
+				
+				$question_option = $row["option_string"] ;
+				echo '<li class="list-group-item">'.$question_option.'</li>' ;
+				
+				
+			  }
+			  
+			echo '</ul>' ;
+		
+		?>
+		<!--				$question_string = $row["question_string"] ;
+				$question_option = $row["option_string"] ;-->
 	</body>
 </html>
--- includes/formdata.inc.php
+++ includes/formdata.inc.php
@@ -2,25 +2,18 @@
 	include_once 'databaseConnection.inc.php';
 	
 	$tag = mysqli_real_escape_string($conn, $_POST['tag']) ;
+	$sql = "select * from form where tag = '$tag' ;";
 	
-	echo $tag ;
+	$result = $conn->query($sql) ;
 	
-	$sql = "select * from form where tag = ? ;";
-	$stmt = mysqli_stmt_init($conn);
-	
-	if(!mysqli_stmt_prepare($stmt, $sql)){
-		echo "sql error" ;
-	}else{
-		mysqli_stmt_bind_param($sql,"s", $tag) ;
-		mysqli_stmt_execute($stmt) ;
-		
-		$result = $stmt->get_result();
-        while ($row = mysqli_fetch_assoc($result))
-        {
-			echo $row['id'] ;
-        }
+	echo x ;
+	while($row = $result->fetch_assoc()){
+		echo $row["id"] ;
+		echo $row["name"] ;
+		echo $row["tag"] ;
+		echo $row["information"] ;
 	}
-	
+
 	header("Location: ../form.php?formdata=success");
 	//header("Location: ../form.php?formdata.inc.php=success");
 ?>
